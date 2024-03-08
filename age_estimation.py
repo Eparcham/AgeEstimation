fe78456dab124d868fde108f49c9b40d7fae1959
ebrahim parcham <e.parcham@fanap.ir>
2024-03-08 14:41:35 +0330

    add transform

--- age_estimation.py
+++ age_estimation.py
@@ -80,3 +80,20 @@ print(df_train.shape, df_test.shape)
 df_train.to_csv("train_data.csv", index=False)
 df_test.to_csv("test_data.csv", index=False)
 df_valid.to_csv("valid_data.csv", index=False)
+
+## train and test transform
+train_transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(degrees = 15),
+    transforms.ColorJitter(brightness=0.2, contrast = 0.2, saturation=0.2, hue = 0.1),
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+])
+
+test_transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+])
+
